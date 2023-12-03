d634adcc42e06dd071b7f0aba8bb21aee47969ef
Mikhail Khromov <khromov05@gmail.com>
2023-12-03 03:51:38 +0300

    few more fixes, actually working now yay

--- backend/app/api/v1/endpoints.py
+++ backend/app/api/v1/endpoints.py
@@ -1,14 +1,6 @@
 import logging
-from typing import Annotated, List
 
-from fastapi import APIRouter, Depends, status
-
-from core.models.models import QuestionAnswer, Test
-from core.service.webhooks import trigger_bot_answers_webhook
-from core.service.service import get_questions_by_test_id
-from core.schemas.user import AnswerRequestModel, QuestionResponseModel
-from core.middlewares.webapp_header import init_data_headers_middleware
-
+from fastapi import APIRouter, status
 
 logger = logging.getLogger(__name__)
 
@@ -16,7 +8,7 @@ router = APIRouter(prefix="/lesson")
 
 
 @router.get(
-    "/route",
+    "/v1-route",
     response_model=dict,
     status_code=status.HTTP_200_OK,
 )
--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,1 +1,1 @@
-gunicorn -w 4 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:80 setup:server
+gunicorn -w 4 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8000 setup:server

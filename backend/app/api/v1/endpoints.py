9d1ff84530291a94384f1f65f6a71d5f13c3cc83
Mikhail Khromov <khromov05@gmail.com>
2023-12-04 03:56:57 +0300

    add routes for list of tickers & ticker info

--- backend/app/api/v1/endpoints.py
+++ backend/app/api/v1/endpoints.py
@@ -1,17 +1,43 @@
+import datetime
 import logging
+from typing import List, Any
 
 from fastapi import APIRouter, status
+from moexalgo import Market, Ticker
+
+from core.exceptions import BadRequest
+from core.schemas.tickers import Period, TickerCandleResponse
 
 logger = logging.getLogger(__name__)
 
-router = APIRouter(prefix="/lesson")
+router = APIRouter()
 
 
 @router.get(
-    "/v1-route",
-    response_model=dict,
+    "/tickers",
+    response_model=List[str],
     status_code=status.HTTP_200_OK,
 )
-async def get_questions():
-    """Get a list of all questions"""
-    return {"message": "Hello World"}
+async def get_tickers():
+    """Get a list of all MOEX tickers"""
+    # TODO: cache this list via nginx or redis
+    stocks = Market('stocks')
+    tickers_pandas = stocks.tickers()
+    tickers = [ticker['SECID'] for ticker in tickers_pandas]
+    return tickers
+
+
+@router.get(
+    "/tickers/{ticker}/",
+    response_model=List[TickerCandleResponse],
+    status_code=status.HTTP_200_OK,
+)
+async def get_price_for_ticker(ticker: str, date_start: datetime.date, date_end: datetime.date, period: Period):
+    """Get price for ticker over time with specified frequency"""
+    try:
+        ticker = Ticker(ticker.upper())
+    except LookupError:
+        raise BadRequest(detail={"error": f"Ticker {ticker.upper()} not found"})
+
+    candles = list(ticker.candles(date=date_start, till_date=date_end, period=period))
+    return candles
---
+++ backend/app/core/schemas/tickers.py
@@ -0,0 +1,26 @@
+import datetime
+from enum import Enum
+
+from pydantic import BaseModel
+
+
+class Period(str, Enum):
+    one_minute = '1m'
+    ten_minutes = '10m'
+    one_hour = '1h'
+    one_day = 'D'
+    one_week = 'W'
+    one_month = 'M'
+    one_quarter = 'Q'
+
+
+class TickerCandleResponse(BaseModel):
+    begin: datetime.datetime
+    end: datetime.datetime
+    close: float
+
+
+class TickerPriceRequest(BaseModel):
+    period: Period
+    date_start: datetime.date
+    date_end: datetime.date

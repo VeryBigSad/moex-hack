b293b4eff4d4fdb7c6d643ad09b32261607fab23
Mikhail Khromov <khromov05@gmail.com>
2023-12-07 18:32:23 +0300

    add tags for docs

--- backend/app/api/v1/__init__.py
+++ backend/app/api/v1/__init__.py
@@ -5,7 +5,7 @@ from .strategy import router as router_strategy
 from .news import router as router_news
 
 router = APIRouter()
-router.include_router(router_tickers, prefix="/tickers")
-router.include_router(router_strategy, prefix="/strategy")
-router.include_router(router_news, prefix="/news")
+router.include_router(router_tickers, prefix="/tickers", tags=["tickers"])
+router.include_router(router_strategy, prefix="/strategy", tags=["strategy"])
+router.include_router(router_news, prefix="/news", tags=["news"])
 

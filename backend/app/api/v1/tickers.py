b933c9d64132442b0e383a9341dc9b65405ae2e6
Mikhail Khromov <khromov05@gmail.com>
2023-12-10 08:05:05 +0300

    fix typo

--- backend/app/api/v1/tickers.py
+++ backend/app/api/v1/tickers.py
@@ -34,38 +34,9 @@ async def get_tickers():
             "price": ticker["PREVLEGALCLOSEPRICE"],
             "name": get_name(ticker["SECID"]) if get_name(ticker["SECID"]) else ticker["SECNAME"],
             "sphere": get_sphere(ticker["SECID"]),
-            "is_positive_forecast": bool(random.randint(0, 1))
+            "is_positive_forecast": get_is_ticker_good(ticker["SECID"])
         }
         for ticker in tickers
-    ]
-    return data
-
-
-@router.get(
-    "/{ticker}/relevant/",
-    response_model=List[RelevantTickerResponse],
-    status_code=status.HTTP_200_OK
-)
-async def get_relevant_tickers(ticker: str):
-    """Get a list of relevant tickers for the specified ticker"""
-    stocks = Market('stocks')
-    tickers = stocks.tickers()
-    tickers = {
-        ticker["SECID"]: ticker
-        for ticker in tickers
-    }
-    small_corr_tickers = get_ticker_corr(ticker.upper())
-    data = [
-        {
-            "ticker": ticker_name,
-            "price": tickers[ticker_name]["PREVLEGALCLOSEPRICE"],
-            "name": get_name(ticker_name) if get_name(ticker_name) else tickers[ticker_name]["SECNAME"],
-            "sphere": get_sphere(ticker_name),
-            "is_positive_forecast": get_is_ticker_good(ticker_name),
-            "correlation_score": corr,
-        }
-        for ticker_name, corr in small_corr_tickers
-        if ticker_name in tickers
     ]
     return data
 
